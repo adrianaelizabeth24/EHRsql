7baa21945ab1dac85357b71d41c6fcbef56d1754
Mayra Ruiz <mey.ruiz27@gmail.com>
2017-10-29 16:36:02 -0600

    Fixes

--- resources/views/reporte_consulta/edit.blade.php
+++ resources/views/reporte_consulta/edit.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('content')
 
+<link href="{{ asset('css/paciente.css')}}" rel="stylesheet">
 
     <form method="post" action="{{action('ReporteConsultaController@update', $id)}}">
         {{csrf_field()}}
