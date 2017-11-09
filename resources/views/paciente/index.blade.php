d6604f1ad1bb77b8fa63a47daab42431c6d4a308
Mayra Ruiz <mey.ruiz27@gmail.com>
2017-11-09 01:27:50 -0600

    Changed nav colors and detalles

--- resources/views/paciente/index.blade.php
+++ resources/views/paciente/index.blade.php
@@ -6,32 +6,32 @@
 	<link href="{{ asset('css/app.css')}}" rel="stylesheet">
 
     <div class="jumbotron">
-        <div class="input-group" style="width:20%;right:56px;padding-right:15px;position:absolute;">
-            <input type="text" class="form-control" placeholder="Buscar paciente">
-            <div class="input-group-btn">
-                <button class="btn btn-default" type="submit">
-                    <i class="glyphicon glyphicon-search"></i>
-                </button>
-            </div>
-        </div>
         <div class="container">
+			<div class="input-group" style="width: 20%;float: right;">
+				<input type="text" class="form-control" placeholder="Buscar paciente">
+				<div class="input-group-btn">
+					<button class="btn btn-default" type="submit">
+						<i class="glyphicon glyphicon-search"></i>
+					</button>
+				</div>
+			</div>
             <h2>Lista de Pacientes</h2>
-        </div>
 
-        <div id="div_pacientes" class="container">
-            @foreach($pacientes as $paciente)
-            <div class="panel panel-default">
-                <div class="panel-heading" role="tab">
-                    <h4 class="panel-title">
-                        <a href="paciente/{{{$paciente->id}}}">{{$paciente->id}}
-                        {{$paciente->nombre}} {{$paciente->apellido_paterno}}  {{$paciente->apellido_materno}}</a>
-                    </h4>
-                </div>
-            </div> <!-- Complete patient info -->
+			<div id="div_pacientes">
+				@foreach($pacientes as $paciente)
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab">
+						<h4 class="panel-title">
+							<a href="paciente/{{{$paciente->id}}}">{{$paciente->id}}
+							{{$paciente->nombre}} {{$paciente->apellido_paterno}}  {{$paciente->apellido_materno}}</a>
+						</h4>
+					</div>
+				</div> <!-- Complete patient info -->
 
-            @endforeach
-            <a href="/paciente/create" class="btn btn-info">Nuevo Paciente</a>
-        </div> <!-- div_pacientes -->
+				@endforeach
+				<a href="/paciente/create" class="btn btn-info">Nuevo Paciente</a>
+			</div> <!-- div_pacientes -->
+		</div>
 
     </div> <!-- jumbotron -->
     <hr>
