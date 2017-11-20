92a8da425c3e730314bd56792bec4a72855ab9fc
Mayra Ruiz <mey.ruiz27@gmail.com>
2017-11-20 16:21:22 -0600

    Fixes

--- resources/views/lugar_residencia/show.blade.php
+++ resources/views/lugar_residencia/show.blade.php
@@ -7,31 +7,31 @@
     <div class="jumbotron">
         <div class="container">
             <h2>Detalle del Lugar Residencia</h2>
-        </div>
-        <br>
-        <div id="div_pacientes" class="container">
-            <div id="patient" role="tabpanel">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="personal_info">
-                            <label>Nombre :</label><p>{{$lugares->nombre}}</p>
-                        </div>
-                    </div>
+			<br>
+			<div id="div_pacientes" class="container">
+				<div id="patient" role="tabpanel">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="personal_info">
+								<label>Nombre :</label><p>{{$lugares->nombre}}</p>
+							</div>
+						</div>
 
-                </div>
-                <br/>
-                <a href="/lugar_residencia" class="btn btn-info">Regresar</a>
-                <form action="{{action('LugarResidenciaController@destroy', $lugares->id)}}" method="post">
-                    {{csrf_field()}}
-                    <input name="_method" type="hidden" value="DELETE">
-                    <button class="btn btn-danger" type="submit">Borrar</button>
-                </form>
+					</div>
+					<br/>
+					<a href="/lugar_residencia" class="btn btn-info">Regresar</a>
+					<form action="{{action('LugarResidenciaController@destroy', $lugares->id)}}" method="post" style="display:unset;">
+						{{csrf_field()}}
+						<input name="_method" type="hidden" value="DELETE">
+						<button class="btn btn-danger" type="submit">Borrar</button>
+					</form>
 
-                <br/>
+					<br/>
 
 
-            </div>
-        </div> <!-- div_pacientes -->
+				</div>
+			</div> <!-- div_pacientes -->
+		</div>
     </div> <!-- jumbotron -->
     <hr>
 @stop
